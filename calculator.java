0988209ccdeea5bed8e36bc34fa9b72fb96f3e7f
ankur dwivedi <ankur.dwivedi199714@gmail.com>
2020-06-03 13:34:07 +0530

    point 4

--- calculator.java
+++ calculator.java
@@ -5,7 +5,9 @@ class calculator{
 		System.out.println("Enter a string value for number");
 		String numbers=br.readLine();
 		calculator ob=new calculator();
+		// System.out.println(ob.Add("//;\n1;2")); for testing uncomment this to write new line in cmd
 		System.out.println(ob.Add(numbers));
+
 	}
 	int Add(String numbers)
 	{
@@ -17,11 +19,27 @@ class calculator{
 			return Integer.parseInt(numbers);
 		else  if(numbers.length()>1)
 		{
-			String regex=",|\n|_";
-			String s[]=numbers.split(regex);
-			for(int x=0;x<s.length;x++){
-				if(!s[x].equals(""))
-				sum=sum+Integer.parseInt(s[x]);
+			if(numbers.substring(0,2).equals("//"))
+			{		
+					String newline="\n";
+					String regex=numbers.substring(2,numbers.indexOf(newline));
+					numbers=numbers.substring(numbers.indexOf(newline)+1);
+					String s[]=numbers.split(regex);
+
+					for(int x=0;x<s.length;x++){
+						if(!s[x].equals(""))
+						sum=sum+Integer.parseInt(s[x]);
+			
+					}	
+			}
+			else{
+				String regex=",|\n|_";
+				String s[]=numbers.split(regex);
+				for(int x=0;x<s.length;x++){
+					if(!s[x].equals(""))
+					sum=sum+Integer.parseInt(s[x]);
+				
+				}		
 			}
 		}
 		return sum;
